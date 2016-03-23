2937c616c4df08617e64be496256c5326cd0d021
titzelle <pseudoups@hotmail.fr>
2016-03-24 00:56:46 +0100

    utilisation de  pour recuperer un fichier json, le montrer dans ShowMoviesController et ajouter des films dans AddMoviesController

--- app/js/controllers/AddMoviesController.js
+++ app/js/controllers/AddMoviesController.js
@@ -2,11 +2,17 @@
  * Created by Emmanuelle on 22/03/2016.
  */
 
-myApp.controller('AddMovieController', function ($scope, movies) {
+myApp.controller('AddMovieController', function ($scope, $http) {
     $scope.submitMovie = function (movie) {
-        $scope.movies=movies;
-        $scope.movies.push(movie);
-        $scope.movie = {};
+        $http.get('movies.json', movie)
+            .success(function(data){
+                $scope.movies=data;
+                $scope.movies.push(movie);
+                $scope.movie = {};
+            })
+            .error(function(resp){
+                alert(resp);
+            });
     };
 });
 
--- app/js/controllers/ShowMoviesController.js
+++ app/js/controllers/ShowMoviesController.js
@@ -1,7 +1,12 @@
 /**
  * Created by Emmanuelle on 22/03/2016.
  */
-myApp.controller('ShowMoviesController', function ($scope,movies) {
-    $scope.movies=movies;
-    console.log(movies);
+myApp.controller('ShowMoviesController', function ($scope,$http) {
+
+    $http.get("movies.json").success(function(data) {
+        $scope.movies=data.movies;
+
+    }).error(function(data, statusText) {
+        alert(statusText);
+    })
 });
