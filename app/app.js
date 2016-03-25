d7e588bcc78158f1b440471ea9bfae46d127bd1e
titzelle <pseudoups@hotmail.fr>
2016-03-25 01:43:38 +0100

    ajout du route provider et configuration, ajout des fragments de pages dans le folder partials, ajout d'une feuille de style, d'une barr de nav commune à toutes les pages

--- app/app.js
+++ app/app.js
@@ -1,10 +1,28 @@
 /**
  * Created by Emmanuelle on 17/03/2016.
  */
-var myApp = angular.module('myApp', ['ngRoute']);
+//var routeAppControllers = angular.module('routeAppControllers', [
+//    'MoviesController',
+//    'MovieFormController'
+//]);
+
+var myApp = angular.module('myApp', [
+    'ngRoute'
+    //'routeAppControllers'
+]);
+
+
 myApp.config(['$routeProvider', function($routeProvider){
     $routeProvider
-        .when('/', {templateUrl:'partials/index.html'})
-        .when('/movies', {templateUrl:"partials/movies.html"})
+        .when('/', {
+            templateUrl:'partials/home.html'
+        })
+        .when('/movies', {
+            templateUrl:"partials/movies.html",
+            //controller:"MoviesController"
+        })
+        .when('/movieForm', {
+            templateUrl:'partials/moviesForm.html'
+        })
         .otherwise({redirectTo : '/'});
 }]);
