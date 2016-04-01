28788d03eb5a03ab9146874b772ac563ffaac77e
titzelle <pseudoups@hotmail.fr>
2016-04-01 20:03:08 +0200

    fichier edition ok

--- app/js/controllers/MovieEditController.js
+++ app/js/controllers/MovieEditController.js
@@ -2,23 +2,25 @@ myApp.controller('MovieEditController', function ($scope, $http, $routeParams, $
 
     $http.get("http://localhost:3000/movies")
         .success(function (data) {
-            $scope.id = $routeParams.id;
-            $scope.movie = data[$scope.id];
+            $scope.movies = data;
+            $scope.movie = $scope.movies[$routeParams.id];
 
-    })
+        })
         .error(function (data, statusText) {
-        alert(statusText);
-    });
+            alert(statusText);
+        });
 
-    $scope.gotoHome = function(path) {
+    $scope.gotoHome = function (path) {
         $location.path(path);
     };
 
-    $scope.editMovie = function (movie){
+    $scope.editMovie = function (movie) {
         console.log("movie", movie);
-        console.log("movies" , $scope.movies)
-        $scope.movie=movie;
-        $scope.movies[$scope.id]=movie;
+        console.log("movies", $scope.movies)
+        $http.put("http://localhost:3000/movies/" + movie.id, movie)
+            .success(function () {
+                $location.path('/movies');
+            })
     };
 
 });
--- app/js/controllers/MoviesController.js
+++ app/js/controllers/MoviesController.js
@@ -1,24 +1,24 @@
 myApp.controller('MoviesController', function ($scope, $http, $location) {
 
     $http.get("http://localhost:3000/movies").success(function (data) {
-        $scope.movies = data;
-        console.log($scope.movies);
-    }).error(function (data, statusText) {
-        alert(statusText);
-    });
+            $scope.movies = data;
+            console.log($scope.movies);
+        })
+        .error(function (data, statusText) {
+            alert(statusText);
+        });
 
-    //$scope.deleteMovie = function (index) {
-    //    $scope.movies.splice(index, 1);
-    //};
 
-    $scope.deleteMovie = function(index){
+
+    $scope.deleteMovie = function (index) {
+        console.log('index', index);
         $http.delete('http://localhost:3000/movies/' + $scope.movies[index].id)
-            .success(function(){
+            .success(function () {
                 $scope.movies.splice(index, 1);
             });
     };
 
-    $scope.gotoEdit =function(path){
+    $scope.gotoEdit = function (path) {
         $location.path(path);
     };
 
